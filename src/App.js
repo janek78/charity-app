0dd37fa4218d5716cd89200c0709e5c204e293ab
Janusz T <janek78@yahoo.com>
2022-05-10 12:23:28 +0200

    basic settings

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './scss/main.scss';
+
+function App() {
+  return (
+      <>
+    <div className="App">
+      Hello
+    </div>
+      <h1>Hello</h1>
+          </>
+  );
+}
+
+export default App;
