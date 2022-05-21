fee8d7826f40c959ed0a8320fd97a2f62a1f6d98
Janusz T <janek78@yahoo.com>
2022-05-21 16:43:33 +0200

    adding logic to component ContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,58 +1,80 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from "react";
 
-export const ContactForm=()=>{
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [isSuccess, setIsSuccess] = useState(false);
+export const ContactForm=()=> {
+    const contactValues = { name: "", email: "", message: "" };
+    const [formValues, setFormValues] = useState(contactValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
-    const data={
-        name,
-        email,
-        message
+    const clearForm=()=>{
+        setFormErrors({});
+        setFormValues(contactValues);
+    }
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
     }
 
-    const handleSubmit =(e)=> {
-        e.preventDefault();
-        fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    const responseError = {
-                    statusText: response.statusText,
-                    status: response.status
-                };
-                    throw (responseError);
-                }
-                if(response.status === 200){
-                    console.log(response.json());
-                    setIsSuccess(true);
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+        const errorMsg=validate(formValues);
+        if(errorMsg.name==="Podane imię jest nieprawidłowe!"||errorMsg.email==='Podany email jest nieprawidłowy!'||errorMsg.message==="Wiadomość musi mieć conajmniej 120 znaków!"||errorMsg.message==="Wiadomość jest za krótka"){
+            setFormErrors(errorMsg);
+            setIsSubmit(false);
+            return
+        }
 
-                }
-            })
-            .catch((error) => {
-                console.log('error', error.status);
-                console.log('error reason', error.statusText);
-            });
+        try {
+            const request={
+                method:'POST',
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify(formValues)
+            };
+            const response = await fetch("https://fer-api.coderslab.pl/v1/portfolio/contact",request);
+            const data=await response.json();
+            setIsSubmit(true);
+            clearForm();
+            console.log(data);
+
+        }catch(error){
+            console.log(error);
+        }
     }
 
+    const validate = formValues => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!formValues.name) {
+            errors.name = "Podane imię jest nieprawidłowe!";
+        }
+        if (!formValues.email) {
+            errors.email = "Podany email jest nieprawidłowy!";
 
+        } else if (!regex.test(formValues.email)) {
+            errors.email = "Podany email jest nieprawidłowy!";
+
+        }
+        if (!formValues.message) {
+            errors.message = "Wiadomość musi mieć conajmniej 120 znaków!";
+
+        } else if (formValues.message.length < 120) {
+            errors.message = "Wiadomość jest za krótka";
+
+        }
+        return errors;
+    };
     return(
 
         <div className='contact__form'>
             <div className="form__header">
                 <h3 className="form__header__title">Skontaktuj się z nami</h3>
                 <div className='decoration'/>
-                {isSuccess && (
+                {isSubmit && (
                     <>
-                        <div className="contact-form-succes">
+                        <div className="submit__success">
                             Wiadomość została wysłana!
                         </div>
-                        <div className="contact-form-succes">
+                        <div className="submit__success">
                             Wkrótce się skontaktujemy.
                         </div>
                     </>
@@ -60,23 +82,22 @@ export const ContactForm=()=>{
             </div>
             <form className='form__content' onSubmit={handleSubmit}>
                 <div className='form__content__details'>
-                    <label htmlFor="name">
+                    <label htmlFor='name'>
                         Wpisz swoje imię
-                        <input type="text" placeholder="Krzysztof" value={name} onChange={(e)=>setName(e.target.value)}/>
-
+                        <input type='text' name='name' id='name' placeholder="Krzysztof" value={formValues.name} onChange={handleChange}/>
+                        <p>{formErrors.name}</p>
                     </label>
                     <label htmlFor="email">
                         Wpisz swój email
-                        <input type="text" placeholder="abc@xyz.pl" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-
+                        <input type="text" name='email' id='email'  placeholder="abc@xyz.pl" value={formValues.email} onChange={handleChange}/>
+                        <p>{formErrors.email}</p>
                     </label>
                 </div>
                 <div className="form__content__description">
                     <label htmlFor="message">
                         Wpisz swoją wiadomość
-                        <textarea type="text" cols='4'value={message} onChange={(e)=>setMessage(e.target.value)} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                        />
-
+                        <textarea type="text" name="message" id='message' cols='4' value={formValues.message} onChange={handleChange} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+                        <p>{formErrors.message}</p>
                     </label>
                     <div className="form__btn">
                         <button type="submit">Wyślij</button>
@@ -86,5 +107,4 @@ export const ContactForm=()=>{
 
         </div>
     )
-
 }
